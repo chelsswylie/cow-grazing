48f35c37009676c28029a3886d04f7f733071e0a
Chelsea Wylie <chelsswylie@gmail.com>
2021-09-06 11:07:09 -0400

    cow is moving with keys, need to connect to array to log results

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// get the image to move across the axes
+let cow = document.querySelector("#animal");
+let moveBy = 10;
+window.addEventListener("load", () => {
+  cow.style.position = "absolute";
+  cow.style.left = 0;
+  cow.style.top = 0;
+});
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+      cow.style.left = parseInt(cow.style.left) - moveBy + "px";
+      break;
+    case "ArrowRight":
+      cow.style.left = parseInt(cow.style.left) + moveBy + "px";
+      break;
+    case "ArrowUp":
+      cow.style.top = parseInt(cow.style.top) - moveBy + "px";
+      break;
+    case "ArrowDown":
+      cow.style.top = parseInt(cow.style.top) + moveBy + "px";
+      break;
+  }
+});
