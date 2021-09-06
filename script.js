dfbf1ce80cbc2cc8bfc6e0ddd5418ec4655b1849
Chelsea Wylie <chelsswylie@gmail.com>
2021-09-06 11:58:46 -0400

    array is working to log values, need to incorporate to take commands from an array. Would like to add a compass feature as well as flip the image of the cow to face whatever direction arrow is pressed

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 let cow = document.querySelector("#animal");
 let moveBy = 10;
+const directionArr = [];
+
 window.addEventListener("load", () => {
   cow.style.position = "absolute";
   cow.style.left = 0;
   cow.style.top = 0;
 });
+
 window.addEventListener("keyup", (e) => {
   switch (e.key) {
     case "ArrowLeft":
       cow.style.left = parseInt(cow.style.left) - moveBy + "px";
-      break;
+      return (
+        console.log("you've gone left by 10px, click again!"),
+        directionArr.push("ArrowLeft"),
+        console.log(directionArr)
+      );
     case "ArrowRight":
       cow.style.left = parseInt(cow.style.left) + moveBy + "px";
-      break;
+      return (
+        console.log("you've gone right by 10px, click again!"),
+        directionArr.push("ArrowRight"),
+        console.log(directionArr)
+      );
     case "ArrowUp":
       cow.style.top = parseInt(cow.style.top) - moveBy + "px";
-      break;
+      return (
+        console.log("you've gone up by 10px, click again!"),
+        directionArr.push("ArrowUp"),
+        console.log(directionArr)
+      );
     case "ArrowDown":
       cow.style.top = parseInt(cow.style.top) + moveBy + "px";
-      break;
+      return (
+        console.log("you've gone down by 10px, click again!"),
+        directionArr.push("ArrowDown"),
+        console.log(directionArr)
+      );
   }
+  console.log(directionArr);
 });
