296f4a49dd87d1af15571d62e408843f1677e2e7
Chelsea Wylie <chelsswylie@gmail.com>
2021-09-07 20:51:51 -0400

    cow flips based on the arrow key pressed

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 let cow = document.querySelector("#animal");
+// let cowUp = "/cowUp.png";
+// let cowRight = "/cowRight.png";
+// let cowBottom = "/cowBottom.png";
+// let cowLeft = "/cowLeft.png";
+// let cow = new cow();
+// cow.src = "/cowUp.png";
 let moveBy = 10;
 const directionArr = [];
-const cowUp = (cow.src = "cow.png");
-const cowRight = (cow.src = "cowRight.png");
-const cowDown = (cow.src = "cowDown.png");
-const cowLeft = (cow.src = "cowLeft.png");
+// const cowUp = (cow.src = "cow.png");
+// const cowRight = (cow.src = "cowRight.png");
+// const cowDown = (cow.src = "cowDown.png");
+// const cowLeft = (cow.src = "cowLeft.png");
 window.addEventListener("load", () => {
   cow.style.position = "absolute";
   cow.style.left = 0;
   cow.style.top = 0;
+  cow.src = "/cowUp.png";
 });
 
 window.addEventListener("keyup", (e) => {
   switch (e.key) {
     case "ArrowLeft":
       cow.style.left = parseInt(cow.style.left) - moveBy + "px";
+      cow.src = "/cowLeft.png";
       return (
         console.log("you've gone left by 10px, click again!"),
         directionArr.push("ArrowLeft"),
@@ -22,6 +30,7 @@ window.addEventListener("keyup", (e) => {
       );
     case "ArrowRight":
       cow.style.left = parseInt(cow.style.left) + moveBy + "px";
+      cow.src = "/cowRight.png";
       return (
         console.log("you've gone right by 10px, click again!"),
         directionArr.push("ArrowRight"),
@@ -29,6 +38,7 @@ window.addEventListener("keyup", (e) => {
       );
     case "ArrowUp":
       cow.style.top = parseInt(cow.style.top) - moveBy + "px";
+      cow.src = "/cowUp.png";
       return (
         console.log("you've gone up by 10px, click again!"),
         directionArr.push("ArrowUp"),
@@ -36,6 +46,7 @@ window.addEventListener("keyup", (e) => {
       );
     case "ArrowDown":
       cow.style.top = parseInt(cow.style.top) + moveBy + "px";
+      cow.src = "/cowBottom.png";
       return (
         console.log("you've gone down by 10px, click again!"),
         directionArr.push("ArrowDown"),
